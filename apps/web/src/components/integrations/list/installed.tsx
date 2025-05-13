5bfb39f42e4833700c44898c3d1918e8c427e80b
Héricles Emanuel <hericles.me@icloud.com>
2025-05-13 17:07:17 -0300

    fix: grids and padding (#231)

--- apps/web/src/components/integrations/list/installed.tsx
+++ apps/web/src/components/integrations/list/installed.tsx
@@ -163,7 +163,7 @@ function CardsView(
   },
 ) {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 px-4 peer">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 peer">
       {integrations.map((integration) => (
         <IntegrationCard
           key={integration.id}
--- apps/web/src/components/integrations/list/marketplace.tsx
+++ apps/web/src/components/integrations/list/marketplace.tsx
@@ -107,7 +107,7 @@ function CardsView(
   },
 ) {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
       {integrations.map((integration) => (
         <Card
           key={integration.id}
