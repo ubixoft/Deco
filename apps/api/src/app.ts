935f268240fa1f845c7ea471d7943d24d03993df
Marcos Candeia <marrcooos@gmail.com>
2025-06-12 19:35:46 -0300

    Improves outbound worker (#523)
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,7 +3,6 @@ import { Hono } from "hono";
 import { timing } from "hono/timing";
 import api from "./api.ts";
 import apps from "./apps.ts";
-import outbound from "./outbound.ts";
 import { AppEnv } from "./utils/context.ts";
 
 export const APPS_DOMAIN_QS = "app_host";
@@ -16,44 +15,26 @@ export const appsDomainOf = (req: Request, url?: URL) => {
     (referer && new URL(referer).searchParams.get(APPS_DOMAIN_QS));
 };
 
-const normalizeHost = (req: Request, env?: AppEnv["Bindings"]) => {
+const normalizeHost = (req: Request) => {
   const host = req.headers.get("host") ?? new URL(req.url).hostname ??
     "localhost";
-
-  const originIsScript = typeof env?.DECO_CHAT_APP_ORIGIN === "string";
-  const isUnauthorized = !req.headers.has("authorization");
-
-  const wellKnownHost = ({
-    [Hosts.API]: Hosts.API,
-    localhost: Hosts.API,
-    "localhost:3001": Hosts.API,
-    "localhost:8000": Hosts.API,
-  })[host];
-
-  const hostIsWellKnown = wellKnownHost !== undefined ||
-    host.endsWith(Hosts.APPS);
-
-  // authorize the request using outbound worker.
-  if (originIsScript && isUnauthorized && wellKnownHost === Hosts.API) {
-    return Hosts.APPS_OUTBOUND;
-  }
-
-  // send to outbound any request that is not to a well known host.
-  if (originIsScript && !hostIsWellKnown) {
-    return Hosts.APPS_OUTBOUND;
-  }
 
   const appsHost = appsDomainOf(req);
   if (appsHost) {
     return Hosts.APPS;
   }
-  return wellKnownHost ?? Hosts.APPS;
+  return {
+    [Hosts.API]: Hosts.API,
+    localhost: Hosts.API,
+    "localhost:3001": Hosts.API,
+    "localhost:8000": Hosts.API,
+  }[host] ?? Hosts.APPS;
 };
 
 export const app = new Hono<AppEnv>({
-  getPath: (req, env) =>
+  getPath: (req) =>
     "/" +
-    normalizeHost(req, env?.env) +
+    normalizeHost(req) +
     req.url.replace(/^https?:\/\/[^/]+(\/[^?]*)(?:\?.*)?$/, "$1"),
 });
 
@@ -61,5 +42,4 @@ app.use(timing({ crossOrigin: true, total: true }));
 
 app.route(`/${Hosts.API}`, api);
 app.route(`/${Hosts.APPS}`, apps);
-app.route(`/${Hosts.APPS_OUTBOUND}`, outbound);
 export default app;
--- apps/outbound/main.ts
+++ apps/outbound/main.ts
@@ -1,6 +1,67 @@
+import { JwtIssuer } from "@deco/sdk/auth";
+import { Hosts } from "@deco/sdk/hosts";
+import { getServerClient } from "@deco/sdk/storage";
+
+export interface Env {
+  DECO_CHAT_APP_ORIGIN: string;
+  ISSUER_JWT_SECRET: string;
+  SUPABASE_URL: string;
+  SUPABASE_SERVER_TOKEN: string;
+}
+
 export default {
-  fetch: (req: Request) => {
-    console.log("outbound", req.url);
-    return fetch(req);
+  fetch: async (req: Request, env: Env) => {
+    const host = req.headers.get("host") ?? new URL(req.url).hostname;
+    if (host !== Hosts.API) { // just forward the request to the target url
+      return fetch(req);
+    }
+
+    // otherwise, we need to authenticate the request
+    const {
+      DECO_CHAT_APP_ORIGIN,
+      ISSUER_JWT_SECRET,
+      SUPABASE_URL,
+      SUPABASE_SERVER_TOKEN,
+    } = env;
+
+    if (
+      !DECO_CHAT_APP_ORIGIN || !ISSUER_JWT_SECRET || !SUPABASE_URL ||
+      !SUPABASE_SERVER_TOKEN
+    ) {
+      return new Response("Missing environment variables", { status: 500 });
+    }
+
+    const db = getServerClient(SUPABASE_URL, SUPABASE_SERVER_TOKEN);
+    const { data, error } = await db
+      .from("deco_chat_hosting_apps")
+      .select("*")
+      .eq("slug", DECO_CHAT_APP_ORIGIN)
+      .maybeSingle();
+
+    if (error) {
+      return new Response("Error querying script", { status: 500 });
+    }
+
+    if (!data) {
+      return new Response("App not found", { status: 404 });
+    }
+
+    const issuer = JwtIssuer.forSecret(ISSUER_JWT_SECRET);
+    const token = await issuer.create({
+      sub: `app:${DECO_CHAT_APP_ORIGIN}`,
+      aud: data?.workspace,
+    });
+
+    const reqHeaders = new Headers(req.headers);
+    reqHeaders.set("Authorization", `Bearer ${token}`);
+
+    return fetch(
+      new Request(req.url, {
+        method: req.method,
+        headers: reqHeaders,
+        body: req.body,
+        redirect: req.redirect,
+      }),
+    );
   },
 };
