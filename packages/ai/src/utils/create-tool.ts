1f21ddab60a1947467346c69050e8472f3971565
Tiago Gimenes <tlgimenes@gmail.com>
2025-06-14 18:21:03 -0300

    chore: reduce tokens (#538)
    * improve conditions
    
    * improve condition

--- packages/ai/src/utils/create-tool.ts
+++ packages/ai/src/utils/create-tool.ts
@@ -50,7 +50,10 @@ export const createTool = ({
           try {
             const result = await execute?.(ctx, options);
 
-            if (Array.isArray(result.content)) {
+            if (
+              Array.isArray(result?.content) && result.content.length > 0 &&
+              result.content[0]?.type === "text"
+            ) {
               // deno-lint-ignore no-explicit-any
               return result.content.map((t: any) => t.text).join("\n\n");
             }
