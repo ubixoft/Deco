0776fdd443bfa7b77fc58d13a63e98e21345b7aa
Marcos Candeia <marrcooos@gmail.com>
2025-06-15 11:09:00 -0300

    Surrounding outbound worker with a trycatch block
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- apps/outbound/main.ts
+++ apps/outbound/main.ts
@@ -36,62 +36,70 @@ export const isWellKnownHost = (host: string): boolean => {
 
 export default {
   fetch: async (req: Request, env: Env) => {
-    const host = req.headers.get("host") ?? new URL(req.url).hostname;
-    const fetcher = isWellKnownHost(host)
-      ? (req: Request, opts?: RequestInit) => env.DECO_CHAT_API.fetch(req, opts)
-      : fetch;
-    if (host !== Hosts.API) { // just forward the request to the target url
+    try {
+      const host = req.headers.get("host") ?? new URL(req.url).hostname;
+      const fetcher = isWellKnownHost(host)
+        ? (req: Request, opts?: RequestInit) =>
+          env?.DECO_CHAT_API?.fetch(req, opts) ?? fetch(req, opts)
+        : fetch;
+      if (host !== Hosts.API) { // just forward the request to the target url
+        return fetcher(
+          req,
+        );
+      }
+
+      // otherwise, we need to authenticate the request
+      const {
+        DECO_CHAT_APP_ORIGIN,
+        ISSUER_JWT_SECRET,
+        SUPABASE_URL,
+        SUPABASE_SERVER_TOKEN,
+      } = env;
+
+      if (
+        !DECO_CHAT_APP_ORIGIN || !ISSUER_JWT_SECRET || !SUPABASE_URL ||
+        !SUPABASE_SERVER_TOKEN
+      ) {
+        return new Response("Missing environment variables", { status: 500 });
+      }
+
+      const db = getServerClient(SUPABASE_URL, SUPABASE_SERVER_TOKEN);
+      const { data, error } = await db
+        .from("deco_chat_hosting_apps")
+        .select("*")
+        .eq("slug", DECO_CHAT_APP_ORIGIN)
+        .maybeSingle();
+
+      if (error) {
+        return new Response("Error querying script", { status: 500 });
+      }
+
+      if (!data) {
+        return new Response("App not found", { status: 404 });
+      }
+
+      const issuer = JwtIssuer.forSecret(ISSUER_JWT_SECRET);
+      const token = await issuer.create({
+        sub: `app:${DECO_CHAT_APP_ORIGIN}`,
+        aud: data?.workspace,
+      });
+
+      const reqHeaders = new Headers(req.headers);
+      reqHeaders.set("Authorization", `Bearer ${token}`);
+
       return fetcher(
-        req,
+        new Request(req.url, {
+          method: req.method,
+          headers: reqHeaders,
+          body: req.body,
+          redirect: req.redirect,
+        }),
+      );
+    } catch (err) {
+      return new Response(
+        `Error: ${err instanceof Error ? err.message : "Unknown error"}`,
+        { status: 500 },
       );
     }
-
-    // otherwise, we need to authenticate the request
-    const {
-      DECO_CHAT_APP_ORIGIN,
-      ISSUER_JWT_SECRET,
-      SUPABASE_URL,
-      SUPABASE_SERVER_TOKEN,
-    } = env;
-
-    if (
-      !DECO_CHAT_APP_ORIGIN || !ISSUER_JWT_SECRET || !SUPABASE_URL ||
-      !SUPABASE_SERVER_TOKEN
-    ) {
-      return new Response("Missing environment variables", { status: 500 });
-    }
-
-    const db = getServerClient(SUPABASE_URL, SUPABASE_SERVER_TOKEN);
-    const { data, error } = await db
-      .from("deco_chat_hosting_apps")
-      .select("*")
-      .eq("slug", DECO_CHAT_APP_ORIGIN)
-      .maybeSingle();
-
-    if (error) {
-      return new Response("Error querying script", { status: 500 });
-    }
-
-    if (!data) {
-      return new Response("App not found", { status: 404 });
-    }
-
-    const issuer = JwtIssuer.forSecret(ISSUER_JWT_SECRET);
-    const token = await issuer.create({
-      sub: `app:${DECO_CHAT_APP_ORIGIN}`,
-      aud: data?.workspace,
-    });
-
-    const reqHeaders = new Headers(req.headers);
-    reqHeaders.set("Authorization", `Bearer ${token}`);
-
-    return fetcher(
-      new Request(req.url, {
-        method: req.method,
-        headers: reqHeaders,
-        body: req.body,
-        redirect: req.redirect,
-      }),
-    );
   },
 };
