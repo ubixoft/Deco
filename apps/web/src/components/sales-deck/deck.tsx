a281e4862399d96318a11fb48dd62e412efa69c2
rafavalls <valls@deco.cx>
2025-06-23 14:28:36 -0300

    Update deck.tsx

--- apps/web/src/components/sales-deck/deck.tsx
+++ apps/web/src/components/sales-deck/deck.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function SalesDeck() {
   useEffect(() => {
     globalThis.location.href =
-      "https://drive.google.com/file/d/1fAxFPJAStVpXd8Tp1hf7DRlkl2kIC8oX/view";
+      "https://drive.google.com/file/d/1ybNf5SrOG8tRhwWYyV7YuKXA_kH1NLyz/view";
   }, []);
 
   return null;
