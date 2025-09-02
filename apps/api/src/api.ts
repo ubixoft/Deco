6557dc44236eaf30f48b666ba8cca6ae2595c863
Marcos Candeia <marrcooos@gmail.com>
2025-09-02 12:07:35 -0300

    Remove unused log
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- apps/api/src/api.ts
+++ apps/api/src/api.ts
@@ -111,10 +111,6 @@ const createMCPHandlerFor = (
         continue;
       }
 
-      if (!("shape" in tool.inputSchema)) {
-        console.log("FAKE", tool.name);
-      }
-
       server.registerTool(
         tool.name,
         {
