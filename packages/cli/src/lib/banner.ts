3895e79d1d71e17a66b42efdf07e44613c642ce1
viktormarinho <56888067+viktormarinho@users.noreply.github.com>
2025-09-03 15:45:45 -0300

    fix: show correct CLI version on deco create (#1144)
    * fix: show correct CLI version on deco create
    
    * ok do not import from cli entrypoint
    
    * fmt

--- packages/cli/src/lib/banner.ts
+++ packages/cli/src/lib/banner.ts
@@ -1,4 +1,17 @@
 import chalk from "chalk";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { join } from "node:path";
+import { readFile } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Read package.json for version
+const decoCliPackageJsonPath = join(__dirname, "../../package.json");
+const decoCliPackageJson = JSON.parse(
+  await readFile(decoCliPackageJsonPath, "utf-8"),
+);
 
 /**
  * Displays a simple DECO ASCII art banner
@@ -14,7 +27,7 @@ export function displayBanner(): void {
 `);
 
   const subtitle = chalk.gray("Creating Deco project");
-  const version = chalk.dim("CLI v0.16.0");
+  const version = chalk.dim(`CLI v${decoCliPackageJson.version}`);
 
   console.log(deco);
   console.log(`  ${subtitle}`);
