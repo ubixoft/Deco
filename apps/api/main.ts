8326ec46ca3b60811b063c0c059ee89dcded0450
Marcos Candeia <marrcooos@gmail.com>
2025-09-05 09:08:20 -0300

    Fixes self domains
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- apps/api/main.ts
+++ apps/api/main.ts
@@ -16,6 +16,7 @@ const instrumentedApp = instrument(app);
 // Domains we consider "self"
 const SELF_DOMAINS: string[] = [
   Hosts.API_LEGACY,
+  Hosts.API,
   // @ts-expect-error env is not typed
   ...(env.VITE_USE_LOCAL_BACKEND ? [] : [Hosts.APPS]),
   // @ts-expect-error env is not typed
