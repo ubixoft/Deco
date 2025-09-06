3563a06b2b08969bd0ef9f4b403d30b41f05fbf1
Tiago Gimenes <tlgimenes@gmail.com>
2025-09-06 12:37:09 -0300

    fix: error compiling schema errors (#1173)

--- packages/runtime/src/mcp-client.ts
+++ packages/runtime/src/mcp-client.ts
@@ -1,11 +1,46 @@
-import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import {
+  Client as BaseClient,
+  ClientOptions,
+} from "@modelcontextprotocol/sdk/client/index.js";
 import {
   SSEClientTransport,
   SSEClientTransportOptions,
 } from "@modelcontextprotocol/sdk/client/sse.js";
 import { WebSocketClientTransport } from "@modelcontextprotocol/sdk/client/websocket.js";
+import { RequestOptions } from "@modelcontextprotocol/sdk/shared/protocol.js";
+import {
+  Implementation,
+  ListToolsRequest,
+  ListToolsResultSchema,
+} from "@modelcontextprotocol/sdk/types.js";
 import { MCPConnection } from "./connection.ts";
 import { HTTPClientTransport } from "./http-client-transport.ts";
+
+/**
+ * WARNNING: This is a hack to prevent schema compilation errors.
+ * More info at: https://github.com/modelcontextprotocol/typescript-sdk/issues/923
+ *
+ * Make sure to keep this updated with the right version of the SDK.
+ * https://github.com/modelcontextprotocol/typescript-sdk/blob/bf817939917277a4c59f2e19e7b44b8dd7ff140c/src/client/index.ts#L480
+ */
+class Client extends BaseClient {
+  constructor(_clientInfo: Implementation, options?: ClientOptions) {
+    super(_clientInfo, options);
+  }
+
+  override async listTools(
+    params?: ListToolsRequest["params"],
+    options?: RequestOptions,
+  ) {
+    const result = await this.request(
+      { method: "tools/list", params },
+      ListToolsResultSchema,
+      options,
+    );
+
+    return result;
+  }
+}
 
 export const createServerClient = async (
   mcpServer: { connection: MCPConnection; name?: string },
