f97838056effc0a3f26aa06b989c32a42b0bafad
viktormarinho <viktormpcs@gmail.com>
2025-09-16 23:08:04 -0300

    chore(ux): do not show recent projects at all if you have none

--- apps/web/src/components/home/organizations.tsx
+++ apps/web/src/components/home/organizations.tsx
@@ -127,51 +127,49 @@ Organizations.Empty = () => (
 function RecentProjectsSection() {
   const recent = useRecentProjects();
 
+  if (recent?.length === 0) {
+    return null;
+  }
+
   return (
     <div className="@container flex flex-col gap-4">
       <h2 className="text-xl font-medium">Recent projects</h2>
-      {recent?.length ? (
-        <div className="grid grid-cols-2 @min-3xl:grid-cols-3 @min-6xl:grid-cols-4 gap-4">
-          {recent.map((project) => (
-            <Link
-              key={`${project.org.slug}/${project.slug}`}
-              to={`/${project.org.slug}/${project.slug}`}
-              className="bg-stone-50 hover:bg-stone-100 transition-colors flex flex-col rounded-lg border border-border"
-            >
-              <div className="p-4 flex flex-col gap-4">
-                <div className="flex justify-between items-start">
-                  <Avatar
-                    url={project.avatar_url || project.org.avatar_url || ""}
-                    fallback={project.org.slug}
-                    size="lg"
-                    objectFit="contain"
-                  />
-                  <Icon
-                    name="chevron_right"
-                    size={20}
-                    className="text-muted-foreground"
-                  />
-                </div>
-                <div className="flex flex-col gap-[2px]">
-                  <h3 className="text-sm text-muted-foreground truncate">
-                    /{project.org.slug}/{project.slug}
-                  </h3>
-                  <p className="font-medium truncate">{project.title}</p>
-                  {project.last_accessed_at && (
-                    <span className="text-xs text-muted-foreground">
-                      Last accessed {timeAgo(project.last_accessed_at)}
-                    </span>
-                  )}
-                </div>
+      <div className="grid grid-cols-2 @min-3xl:grid-cols-3 @min-6xl:grid-cols-4 gap-4">
+        {recent.map((project) => (
+          <Link
+            key={`${project.org.slug}/${project.slug}`}
+            to={`/${project.org.slug}/${project.slug}`}
+            className="bg-stone-50 hover:bg-stone-100 transition-colors flex flex-col rounded-lg border border-border"
+          >
+            <div className="p-4 flex flex-col gap-4">
+              <div className="flex justify-between items-start">
+                <Avatar
+                  url={project.avatar_url || project.org.avatar_url || ""}
+                  fallback={project.org.slug}
+                  size="lg"
+                  objectFit="contain"
+                />
+                <Icon
+                  name="chevron_right"
+                  size={20}
+                  className="text-muted-foreground"
+                />
               </div>
-            </Link>
-          ))}
-        </div>
-      ) : (
-        <div className="text-sm text-muted-foreground">
-          No recent projects. Create one now.
-        </div>
-      )}
+              <div className="flex flex-col gap-[2px]">
+                <h3 className="text-sm text-muted-foreground truncate">
+                  /{project.org.slug}/{project.slug}
+                </h3>
+                <p className="font-medium truncate">{project.title}</p>
+                {project.last_accessed_at && (
+                  <span className="text-xs text-muted-foreground">
+                    Last accessed {timeAgo(project.last_accessed_at)}
+                  </span>
+                )}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
