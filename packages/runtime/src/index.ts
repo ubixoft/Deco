685b3ce45ae24bb7a1a9cd721f4a347a6079ad77
guitavano <tavano62@gmail.com>
2025-10-09 14:15:20 -0300

    Revert "fix: now it does not broke with non jwt tokens (#1403)" (#1417)
    This reverts commit 83969c129f6b8640c20b5a01ce20384cdf7fae49.

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -233,12 +233,11 @@ export const withBindings = <TEnv>({
   const apiUrl = env.DECO_API_URL ?? "https://api.decocms.com";
   let context;
   if (typeof tokenOrContext === "string") {
-    const isJwt = tokenOrContext.split(".").length === 3;
-    const decoded = isJwt ? decodeJwt(tokenOrContext) : {};
+    const decoded = decodeJwt(tokenOrContext);
     const workspace = decoded.aud as string;
 
     context = {
-      state: decoded?.state as Record<string, unknown>,
+      state: decoded.state as Record<string, unknown>,
       token: tokenOrContext,
       workspace,
       ensureAuthenticated: AUTHENTICATED(decoded.user, workspace),
